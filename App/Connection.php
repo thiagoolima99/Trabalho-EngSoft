cfdcb56cf28406162220b36c9e112b1a8fe5c01a
Thiago Lima <thiago.c7s@hotmail.com>
2021-07-12 20:28:20 -0300

    db connection

--- App/Connection.php
+++ App/Connection.php
@@ -5,7 +5,7 @@
 		public static function getDb() {
 			try {
 				$conn = new \PDO(
-					"mysql:host=localhost;dbname=agenda_contatos;charset=utf8","root", ""
+					"mysql:host=ec2-23-20-124-77.compute-1.amazonaws.com;dbname=agenda_contatos;charset=utf8","zrukxewzxeqxnv", "7447e094b9f100122a544f53b992a16d7049df8fce58e092d5f1e3ac83b75a8b"
 				);
 
 				return $conn;
