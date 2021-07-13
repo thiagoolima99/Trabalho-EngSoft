0537b5c8fea8f43a090540304fd3fd13cf00384e
Thiago Lima <thiago.c7s@hotmail.com>
2021-07-13 12:26:16 -0300

    teste
    teste

--- App/Connection.php
+++ App/Connection.php
@@ -16,16 +16,4 @@
 			}
 		}
 	}
-
-		//Get Heroku ClearDB connection information
-	/*$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
-	$cleardb_username = $cleardb_url["b0b70ddaecd9cc"];
-	$cleardb_password = $cleardb_url["603c4f15"];
-	$cleardb_db = substr($cleardb_url["heroku_c46bfa8379ea968"],1);
-	$active_group = 'default';
-	$query_builder = TRUE;
-		// Connect to DB
-	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);*/
-
 ?>
